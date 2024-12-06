52809b50b3aa5d926d38aeffac95ff0576fa4737
IT22074072 <it22074072@my.sliit.lk>
2024-12-06 11:02:24 +0530

    fully backend check with the server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 
+
 app.use(express.json()); //allows us to accept JSON data in the req.body- middleware
+
+
+app.get("/api/products" ,async (req, res) =>{
+  try {
+    const products = await Product.find({});
+    res.status(200).json({success: true, data: products})
+  } catch (error) {
+    console.log("Error in fetching products: ", error.message);
+    res.status(500).json({success:false, message:"Server Error"})
+
+  }
+
+
+})
 
 app.post("/api/products", async (req, res) => {
   const product = req.body; //user will send this data as a request
@@ -27,6 +43,43 @@ app.post("/api/products", async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 });
+
+app.delete("/api/products/:id", async(req, res) => {
+  const {id} = req.params; //passing the product is through params]
+  console.log("id:", id);
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({success:true, message:"Product deleted Successfully!"})
+
+  } catch (error) {
+    //if not found the id
+    console.log("Error in deleting products: ", error.message);
+    res.status(404).json({success:false, message: "Product not found"})
+  }
+
+})
+
+
+app.put("/api/products/:id", async (req, res)=>{
+  const {id} = req.params;
+
+  const product = req.body;  //updated body
+
+  //404 error if id not found
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({success:false, message:"Product not found"})
+
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true} );
+    res.status(200).json({success:true, data: updatedProduct})
+  } catch (error) {
+    //internal server error
+    res.status(500).json({success:false, message:"Server Error"})
+  }
+
+})
 
 console.log(process.env.MONGO_URI);
 
