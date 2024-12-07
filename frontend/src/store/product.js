5c655debe9a531f6fb558c601ab8992204b4a624
IT22074072 <it22074072@my.sliit.lk>
2024-12-07 19:17:22 +0530

    complete delete product

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -18,18 +18,32 @@ export const useProductStore = create((set) => ({
     const data = await res.json();
     set((state) => ({ products: [...state.products, data.data] }));
     return { success: true, message: "Product created successfully!" };
-
   },
   fetchProducts: async () => {
     const res = await fetch("/api/products", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        //body: JSON.stringify(newProduct), - remove the body part cause get method have no body
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      //body: JSON.stringify(newProduct), - remove the body part cause get method have no body
     });
 
     const data = await res.json();
-    set({products:data.data });
-  }
+    set({ products: data.data });
+  },
+  deleteProduct: async (pid) => {
+    const res = await fetch(`/api/products/${pid}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    if (!data.success) return { success: false, message: data.message };
+
+    //update the ui immediately
+    set((state) => ({products: state.products.filter((product) => product._id !== pid),
+    }));
+    return { success: true, message: data.message };
+  },
 }));
